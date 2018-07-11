209cfd23415e2a455608ae1399fd5bc7074a1629
Konrad Beiske <konrad@beiske.org>
2018-07-11 11:09:55 +0200

    Extra debug info

--- test/framework/src/main/java/org/elasticsearch/test/rest/yaml/ESClientYamlSuiteTestCase.java
+++ test/framework/src/main/java/org/elasticsearch/test/rest/yaml/ESClientYamlSuiteTestCase.java
@@ -311,17 +311,21 @@ public abstract class ESClientYamlSuiteTestCase extends ESRestTestCase {
         Version version = null;
         for (int i = 0; i < numHosts; i++) {
             //we don't really use the urls here, we rely on the client doing round-robin to touch all the nodes in the cluster
-            Response response = restClient.performRequest("GET", "/");
-            ClientYamlTestResponse restTestResponse = new ClientYamlTestResponse(response);
-            Object latestVersion = restTestResponse.evaluate("version.number");
-            if (latestVersion == null) {
-                throw new RuntimeException("elasticsearch version not found in the response");
-            }
-            final Version currentVersion = Version.fromString(latestVersion.toString());
-            if (version == null) {
-                version = currentVersion;
-            } else if (version.onOrAfter(currentVersion)) {
-                version = currentVersion;
+            try {
+                Response response = restClient.performRequest("GET", "/");
+                ClientYamlTestResponse restTestResponse = new ClientYamlTestResponse(response);
+                Object latestVersion = restTestResponse.evaluate("version.number");
+                if (latestVersion == null) {
+                    throw new RuntimeException("elasticsearch version not found in the response");
+                }
+                final Version currentVersion = Version.fromString(latestVersion.toString());
+                if (version == null) {
+                    version = currentVersion;
+                } else if (version.onOrAfter(currentVersion)) {
+                    version = currentVersion;
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Something failed, i="+i+" and numHosts="+numHosts, e);
             }
         }
         return version;
